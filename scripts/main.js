3542bbc81c4ffce43b8b6a60f60d2017b39c728d
mandrewcito <anbaalo@gmail.com>
2015-07-13 19:05:13 +0200

    simple http para pruebas

--- scripts/main.js
+++ scripts/main.js
@@ -27,14 +27,69 @@ var richText = new PIXI.Text('COMUNCAS GAME DEV ',style);
 richText.x = window.innerWidth*2/6;
 richText.y = basicText.y+60;
 
+
+// add it to the stage
+var boton=creaBoton();
+stage.addChild(boton);
 stage.addChild(richText);
-stage.addChild(sprite);
+//stage.addChild(sprite);
 // start animating
 animate();
+
+function creaBoton(){
+
+/********************************************/
+var button = new PIXI.Sprite.fromImage('../img/button.png');
+button.buttonMode = true;
+button.position.x = window.innerWidth*2/6;
+button.position.y = window.innerHeight*2/6;
+// make the button interactive...
+button.interactive = true;
+    button
+        // set the mousedown and touchstart callback...
+        .on('mousedown', onButtonDown)
+        .on('touchstart', onButtonDown)
+
+        // set the mouseup and touchend callback...
+        .on('mouseup', onButtonUp)
+        .on('touchend', onButtonUp)
+        .on('mouseupoutside', onButtonUp)
+        .on('touchendoutside', onButtonUp)
+
+        // set the mouseover callback...
+        .on('mouseover', onButtonOver)
+
+        // set the mouseout callback...
+        .on('mouseout', onButtonOut)
+
+        // you can also listen to click and tap events :
+        //.on('click', noop)
+        return button;
+}
 
 function animate() {
 
     requestAnimationFrame(animate);
     // render the root container
     renderer.render(stage);
+}
+
+function onButtonDown()
+{
+  console.log("onButtonDown");
+}
+
+function onButtonUp()
+{
+ console.log("onButtonUp");
+}
+
+function onButtonOver()
+{
+ console.log("onButtonOver");
+}
+
+function onButtonOut()
+{
+    console.log("onButtonOut");
 }
